d03f5e533a8542b332579d2f203ee291c0727283
Attie Grande <attie@attie.co.uk>
2009-10-26 11:44:42 +0000

    added license banners to source files

--- api.h
+++ api.h
@@ -1,3 +1,23 @@
+/*
+    libxbee - a C library to aid the use of Digi's Series 1 XBee modules
+              running in API mode (AP=2).
+
+    Copyright (C) 2009  Attie Grande (attie@attie.co.uk)
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 struct t_data {
   unsigned char data[128];
   unsigned int length;
@@ -27,8 +47,6 @@ struct {
 
   char *path;
 } xbee;
-
-#define MAX_PACKETS 1024
 
 void *Xmalloc(size_t size);
 void *Xrealloc(void *ptr, size_t size);
